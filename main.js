4a3c16a7984121b014c144e283ad610f09dd951a
Thomas Binetruy <tbinetruy@gmail.com>
2018-09-30 16:55:42 +0200

    added readline

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const app = require("./index.js");
+const readline = require("readline");
 const helpers = require("./helpers.js");
 
 const ptr = helpers.createString(app, "* 1 2 3 4 5 6");
-// logString(memoryBuffer, app.read(ptr));
-console.log("app result => ", app.read(ptr));
+helpers.logString(app.memory.buffer, ptr);
+console.log("app result => ", app.read(ptr), ptr);
+console.log("app result bis => ", app.read(ptr), ptr);
 
-console.log("done");
+const ptr2 = helpers.createString(app, "* 1 2 3 4 5 6");
+console.log("app result => ", app.read(ptr2));
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(">> ", answer => {
+  const ptr = helpers.createString(app, "* 1 2 3 4");
+  console.log("foo", app.read(ptr));
+
+  rl.close();
+});
